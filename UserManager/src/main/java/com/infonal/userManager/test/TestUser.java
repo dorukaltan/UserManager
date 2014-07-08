44602e1ac7487d92a66b118711688163101d47cc
Doruk <daltan@altar-tr.com>
2014-07-08 03:44:49 +0300

    update on junit test

--- UserManager/src/main/java/com/infonal/userManager/test/TestUser.java
+++ UserManager/src/main/java/com/infonal/userManager/test/TestUser.java
@@ -6,16 +6,89 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.omg.PortableInterceptor.SUCCESSFUL;
 
+import com.infonal.userManager.helper.UserHelper;
+import com.infonal.userManager.model.User;
+
 public class TestUser {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+//	@Test
+//	public void test() {
+//		
+//		fail("Not yet implemented");
+//	}
+	
 	@Test
-	public void test() {
-		
-		fail("Not yet implemented");
+	public void testCreateUser() {
+		User user = new User();
+		user.setName("test1");
+		user.setLastname("test1");
+		if(UserHelper.saveUser(user))
+		{
+			System.out.println("user created successfully: " + user.getId());
+			if(!UserHelper.removeUser(user))
+				System.out.println(user.getId() + " user could not be removed.");;
+		}
+		else
+			fail("User could not be created");
 	}
+	
+	@Test
+	public void testUpdateUser() {
+		User user = new User();
+		user.setName("test1");
+		user.setLastname("test1");
+		if(UserHelper.saveUser(user))
+		{
+			user.setName("test2");
+			if(UserHelper.saveUser(user))
+				System.out.println("user updated successfully");
+			else
+				fail("User could not be updated");
+			if(!UserHelper.removeUser(user))
+				System.out.println(user.getId() + " user could not be removed.");
+		}
+		else
+			fail("User could not be created in order to test update");
+	}
+	
+	@Test
+	public void testGetUser() {
+		User user = new User();
+		user.setName("test1");
+		user.setLastname("test1");
+		if(UserHelper.saveUser(user))
+		{
+			User user2 = UserHelper.getUser(user.getId());
+			if(user2 == null || !user2.getName().equals(user.getName()))
+				fail("getUser error");
+			else
+				System.out.println("userGet succeeded");
+
+			if(!UserHelper.removeUser(user))
+				System.out.println(user.getId() + " user could not be removed.");
+		}
+		else
+			fail("User could not be created in order to test select");
+	}
+	
+	@Test
+	public void testRemoveUser() {
+		User user = new User();
+		user.setName("test1");
+		user.setLastname("test1");
+		if(UserHelper.saveUser(user))
+			if(UserHelper.removeUser(user))
+				System.out.println("User removed successfully");
+			else
+				fail("User could not be removed");
+		else
+			fail("User could not be created");
+		
+	}
+	
 
 }
